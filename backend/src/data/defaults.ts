374eb94086cf0bfa273c6b297dd9bd4ccdd84504
eggy <eggyrules@gmail.com>
2022-04-17 20:54:19 -0400

    Remove unnecessary backend bits

--- backend/src/data/defaults.ts
+++
@@ -1,8 +0,0 @@
-// if ocr has >= this threshold, that player wins
-export const WIN_CONFIDENCE_THRESHOLD = 0.65;
-
-export const NUM_ROUNDS = 3;
-
-export const MAX_DRAWS_BEFORE_SYNC = 5;
-
-export const SYNC_INTERVAL_MS = 1000;
--- backend/src/utils/rng.ts
+++
@@ -1,5 +0,0 @@
-export function randomGuildId(): string {
-  return Math.round(Math.random() * 100000)
-    .toString()
-    .padStart(6, "0");
-}
