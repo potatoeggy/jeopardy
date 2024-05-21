86aa12c4ef8e92ef96afcecfdc2ca23c133b67a2
eggy <d7chen@uwaterloo.ca>
2024-05-21 15:01:56 -0400

    content: add biol 240 module 1

---
+++ frontend/src/data/games/biol240Module1.ts
@@ -0,0 +1,125 @@
+import type { Game } from "../games";
+export default <Game>{
+  title: "BIOL 240: Module 1 & 2",
+  board: [
+    {
+      heading: "Ancients",
+      questions: [
+        [
+          "Cyanobacteria were the first to use light and water to generate this compound essential for life today.",
+          "What is oxygen?",
+        ],
+        [
+          "These two organelles were thought to have originated from endosymbiosis.",
+          "What are mitochondria and chloroplasts?",
+        ],
+        ["The process of converting RNA to proteins.", "What is translation?"],
+        [
+          "These two scientists simulated the primordial conditions to produce the first organic compounds.",
+          "Who were Miller and Urey?",
+        ],
+        [
+          "Pasteur disproved this long-standing theory by sterilising broth in a swan-neck flask.",
+          "What is spontaneous generation?",
+        ],
+      ],
+    },
+    {
+      heading: "Inside the Cell",
+      questions: [
+        [
+          "Eukaryotes and archaea, but not bacteria, have these proteins to compress DNA in their nuclear regions.",
+          "What are histones?",
+        ],
+        [
+          "These hair-like structures can sometimes be found between the cell membrane and the cell wall and are used for motility.",
+          "What are flagella?",
+        ],
+        [
+          "This enzyme helps compress DNA by cutting it, coiling it, then reattaching it back together.",
+          "What is topoisomerase?",
+        ],
+        [
+          "These accumulations of carbon, sulfur, and other compounds inside cells allow them to store them for later.",
+          "What are inclusion bodies?",
+        ],
+        [
+          "This protein is present in the middle of the cell during division to help catalyse breaking and reforming the cell wall.",
+          "What is FtsZ?",
+        ],
+      ],
+    },
+    {
+      heading: "Around the Cell",
+      questions: [
+        [
+          "This structural layer gives cells their shape and protects them from mechanical forces.",
+          "What is the cell wall?",
+        ],
+        [
+          "Unlike active transport, facilitated diffusion does not require this compound to transport molecules across the cell membrane.",
+          "What is ATP?",
+        ],
+        [
+          "This type of motility involves laying down slime and then using protein feet to pull the cell forward.",
+          "What is gliding?",
+        ],
+        [
+          "These protein channels in the outer membrane move nutrients of higher concentration outside into cells.",
+          "What are porins?",
+        ],
+        [
+          "Gram-positive bacteria contain this negatively charged compound to hold together peptidoglycan.",
+          "What is teichoic acid?",
+        ],
+      ],
+    },
+    {
+      heading: "Biochemistry",
+      questions: [
+        [
+          "This protective shell around almost all cells is composed of a bilayer of this type of compound.",
+          "What are phospholipids?",
+        ],
+        [
+          "Nucleic acids are composed of these two subunits to produce polypeptides and reproduce.",
+          "What are (deoxy)ribonucleotides?",
+        ],
+        [
+          "These catalytic nucleic acids are able to excise, splice, and even replicate themselves.",
+          "What are ribozymes?",
+        ],
+        ["Byproducts of fermentation.", "What are lactate or ethanol and CO₂?"],
+        [
+          "A unique amino acid isomer that is only found in the peptidoglycan cross links of cell walls.",
+          "What is D?",
+        ],
+      ],
+    },
+    {
+      heading: "Bacteria",
+      questions: [
+        [
+          "This region inside a bacterium contains its genetic information.",
+          "What is the nucleoid?",
+        ],
+        [
+          "These branching filaments of some rod-shaped bacteria eventually progress into mycelia.",
+          "What are hyphae?",
+        ],
+        [
+          "The region between the cytoplasmic membrane and the cell wall in all bacteria.",
+          "What is the periplasm?",
+        ],
+        [
+          "Magnetosomes contain chains of this mineral, serving as an internal compass for bacteria that possess it.",
+          "What is magnetite?",
+        ],
+        [
+          "The step of cell wall formation that penicillin interferes with by inhibiting the FtsI protein.",
+          "What is cross-linking / transpeptidation?",
+        ],
+      ],
+    },
+  ],
+};
