c5def0a031147a3c599590b115c04bf53257a3f6
eggy <d7chen@uwaterloo.ca>
2024-06-04 14:47:05 -0400

    content: add biol 240 module 2

---
+++ frontend/src/data/games/biol240Module3.ts
@@ -0,0 +1,128 @@
+import type { Game } from "../games";
+export default <Game>{
+  title: "BIOL 240: Module 3 & 4",
+  board: [
+    {
+      heading: "Archaea",
+      questions: [
+        [
+          "Halophilic archaea pump in this alkali metal ion to prevent water from leaving the cell.",
+          "What is potassium?",
+        ],
+        [
+          "Methylotrophic methanogens produce this compound as a metabolic byproduct.",
+          "What is methane?",
+        ],
+        [
+          "Compared to bacteria and eukaryotes, this cellular structure is chemically distinct in archaea.",
+          "What is the plasma membrane?",
+        ],
+        [
+          "An archaeal cell wall polymer not present in bacteria.",
+          "What is pseudopeptidoglycan / psuedomurein?",
+        ],
+        [
+          "The amino acid isomer not present in archaeal cell walls but present in bacterial cell walls.",
+          "What is D?",
+        ],
+      ],
+    },
+    {
+      heading: "Eukaryotes",
+      questions: [
+        [
+          "Mitochondria and chloroplasts both use this collection of proteins and molecules to generate ATP via chemiosmosis",
+          "What is the electron transport chain?",
+        ],
+        [
+          "This polyphetic group of single-celled eukaryotes typically do not have cell walls.",
+          "What are protozoa / slime molds / etc?",
+        ],
+        [
+          "Bacteria contain hopanoids for rigidity and stability. Eukaryotes have this equivalent compound instead.",
+          "What are sterols?",
+        ],
+        [
+          "This component in microtubules is used to separate chromosomes during cell division and acts as a homologue to bacteria's FtsZ.",
+          "What is tubulin?",
+        ],
+        [
+          "These bonds between sugars in cellulose and chitin give it the strength and rigidity necessary for their cell walls",
+          "What are beta-1,4-glycosidic bonds?",
+        ],
+      ],
+    },
+    {
+      heading: "Overly-Specific Species Names",
+      questions: [
+        [
+          'Haloquadratum walsbyi has "quad" in its name because it has this property.',
+          "What is square-shaped?",
+        ],
+        [
+          'This genus of anaerobic protozoan parasites is a model protozoa and causes "hiker\'s diarrhea".',
+          "What is Giardia?",
+        ],
+        [
+          "This archaeal phylum has exactly one member, which symbiotes with Ignicoccus.",
+          "What is Nanoarchaeota?",
+        ],
+        [
+          "This fungus caused the Irish potato famine.",
+          "What is Phytophthora?",
+        ],
+        [
+          "This archeon likes very high salt concentrations and its name is reminiscent of when it was originally classified as a bacterium.",
+          "What is Halobacterium salinarum?",
+        ],
+      ],
+    },
+    {
+      heading: "Better Know Latin",
+      questions: [
+        ["These eukaryotes lack mitochondria.", "What are amitochondriates?"],
+        [
+          "These organisms consume organic matter from other organisms, instead of producing it via photosynthesis.",
+          "What are heterotrophs?",
+        ],
+        [
+          'The term for these hollow tube networks produced by some archaea is borrowed from the Latin phrase meaning "small reed".',
+          "What are cannulae?",
+        ],
+        [
+          'This archaeal phylum was named from the Greek word for "wonder" because it was surprising to learn they oxidised ammonia.',
+          "What is Thaumarchaeota?",
+        ],
+        [
+          'Fungi undergo meiosis under stress in this membrane-enclosed cell, whose term originates from the Greek word for "skin bag".',
+          "What is an ascus?",
+        ],
+      ],
+    },
+    {
+      heading: "Weird and Wonderful",
+      questions: [
+        [
+          "This method of reproduction possible in eukaryotes creates haploid cells with only one set of chromosomes.",
+          "What is meiosis?",
+        ],
+        [
+          "This theory that mitochondria and chloroplasts were originally bacteria that were ingested by eukaryotes is now commonly accepted today.",
+          "What is endosymbiosis?",
+        ],
+        [
+          'These delightful eukaryotes are the only ones with a social cycle, where they can form multicellular "slugs".',
+          "What are slime molds?",
+        ],
+        [
+          'These cute little proteins "walk" on microtubules to transport vesicles to other parts of the cell.',
+          "What are kinesins / dyneins?",
+        ],
+        [
+          "Cordyceps are a family of fungal parasite that infects this kind of insect by growing a fruiting body out of its head.",
+          "What are arthropods?",
+        ],
+      ],
+    },
+  ],
+};
