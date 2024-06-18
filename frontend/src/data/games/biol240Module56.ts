c53c77855128a6ed0c3ccd825232e0bc65befe8a
eggy <d7chen@uwaterloo.ca>
2024-06-18 10:56:54 -0400

    content: add biol 240 module 5/6

---
+++ frontend/src/data/games/biol240Module56.ts
@@ -0,0 +1,128 @@
+import type { Game } from "../games";
+export default <Game>{
+  title: "BIOL 240: Module 5 & 6",
+  board: [
+    {
+      heading: "Viruses",
+      questions: [
+        [
+          "Non-enveloped viruses escape from this organelle after endocytosis to gain access to host cells.",
+          "What is the endosome?",
+        ],
+        [
+          'This type of virus never actually enters its host, instead "injecting" its DNA directly into the cytoplasm.',
+          "What is a bacteriophage?",
+        ],
+        [
+          "Giant viruses are only found in this type of organism.",
+          "What are amoebas?",
+        ],
+        [
+          "These proteins self-assemble to form viral capsids.",
+          "What are capsomers?",
+        ],
+        [
+          "This type of bacteriophage integrates its genome into the host and lets it divide a bunch before lysing.",
+          "What is a temperate bacteriophage?",
+        ],
+      ],
+    },
+    {
+      heading: "The Wrong Phrase",
+      questions: [
+        [
+          "Viroids are composed of one strand of RNA with no loose ends, with neither proteins nor an envelope. They are found in archaea and bacteria.",
+          "What is they are only found in plants?",
+        ],
+        [
+          "The plaque assay prepares a serial dilution of viruses onto a lawn of host cells to determine the total number of virus particles present.",
+          "What is they only count infectious particles?",
+        ],
+        [
+          "A prophage is a bacteriophage genome integrated into the circular bacterial genome but not as a plasmid, replicating with the bacteria until stressed.",
+          "What is they can also be found as plasmids?",
+        ],
+        [
+          "To replicate, a virus must enter a cell, release their genome, integrate their genome into the host's, replicate their genome, produce virus proteins, assemble new viruses, then exit the cell.",
+          "What is they do not have to integrate their genome?",
+        ],
+        [
+          "Peptone is a defined medium that contains lots of macro- and micronutrients whose makeup is not chemically defined.",
+          "What is peptone is a complex medium?",
+        ],
+      ],
+    },
+    {
+      heading: "Conditions",
+      questions: [
+        ["The water activity of pure water.", "What is 1?"],
+        [
+          "A media of this state of matter is useful to grow culture to high densities",
+          "What is liquid media?",
+        ],
+        [
+          "Proteins undergo this process past their optimum temperature.",
+          "What is denaturation?",
+        ],
+        [
+          "Psychrotolerant organisms are able to grow ~0°C but grow optimally at this temperature (any temp in range is accepted).",
+          "What is 20–40°C?",
+        ],
+        [
+          "Catalase is an enzyme that protects cells against this reactive oxygen species.",
+          "What is hydrogen peroxide?",
+        ],
+      ],
+    },
+    {
+      heading: "Hard Words",
+      questions: [
+        [
+          "This name for a circular clearing in a lawn of bacteria from bacteriophages is also the name of fatty deposits in arteries.",
+          "What is a plaque?",
+        ],
+        [
+          "This form of metabolism consumes energy to turn compounds into macromolecules.",
+          "What is anabolism?",
+        ],
+        [
+          "This form of analysis identifies viruses by checking if they adhere to red blood cells.",
+          "What is a hemagglutination assay?",
+        ],
+        [
+          "This -troph uses light for energy, inorganic electron sources, and inorganic carbon sources.",
+          "What is a photolithoautotroph?",
+        ],
+        [
+          "This category of microorganism requires oxygen to grow, but only at low concentrations.",
+          "What is a microaerophile?",
+        ],
+      ],
+    },
+    {
+      heading: "BIOL 240L",
+      questions: [
+        [
+          "This endpoint assay assesses the dilution of viruses required to kill 50% of test animals.",
+          "What is LD₅₀?",
+        ],
+        [
+          "This type of centrifugation uses the different densities of viral components and particles to solutions to separate particles into layers.",
+          "What is gradient centrifugation?",
+        ],
+        [
+          "This method of quantifying viruses uses an electron microscope to determine the number of virus particles present in a sample, but cannot differentiate infectious particles.",
+          "What is direct counting?",
+        ],
+        [
+          "Enrichment media encourages certain microbes to grow but not others by this mechanism.",
+          "What is by not providing nutrients for the others?",
+        ],
+        [
+          "The pour plate method adds cells to the agar solution before pouring them into plates but requires the organism to have this property in order to grow.",
+          "What is thermotolerance?",
+        ],
+      ],
+    },
+  ],
+};
